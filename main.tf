6d243ed42adaf8479183d1dcc688d9284765569e
pradnya1999-acn <159138875+pradnya1999-acn@users.noreply.github.com>
2024-02-26 20:10:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+#   version = "= 3.55.0"
+  credentials = var.sa_key
+  #project = var.project_id
+}
+ 
+variable "sa_key" {
+  default =  ""
+}
+ 
+# resource "google_storage_bucket_iam_binding" "binding" {
+#   bucket = "test-secret-rotation-fuction"
+#   #project = "parabolic-base-409505"
+#   role = "roles/storage.legacyBucketOwner"
+#   members = [
+#     "user:jane@example.com",
+#   ]
+# }
+ 
+resource "google_storage_bucket" "auto-expire" {
+  name          = "no-public-access-bucket2463"
+  location      = "US"
+  force_destroy = true
+   retention_policy {
+     retention_period = 2145724
+     is_locked = true
+   }
+  public_access_prevention = "enforced"
+  encryption {
+    default_kms_key_name = "projects/parabolic-base-409505/locations/us-central1/keyRings/secret-keyring/cryptoKeys/secret-key"
+  }
+}
